7de3ef05488d53850cfa882fbd1e96118bc2d15b
Abiel Ordonez <iordonez@zamorano.edu>
2026-07-08 08:32:10 -0600

    wip

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from utils.estilos import cargar_css
 
 
 st.set_page_config(
-    page_title="VMD",
+    page_title="ZVMD",
     page_icon="assets/logo.png",
     layout="wide"
 )
@@ -21,7 +21,7 @@ with col_logo:
     st.image("assets/Universidad-Zamorano-Logo-1024x220.png", use_container_width=True)
 
 with col_titulo:
-    st.title("VALIDADOR DE MIGRACIÓN DE DATOS (VMD)")
+    st.title("VALIDADOR DE MIGRACIÓN DE DATOS (ZVMD)")
 
 opcion = st.sidebar.radio(
     "Módulo",
